7c6d1b9e4379c677137e6fafe51e798335445132
Addison Hill <adddison.hill@gmail.com>
2019-12-10 15:19:55 -0600

    added knexfile, migrations, and seeds

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("<h3>Car Dealership Database</h3>");
+});
+
+module.exports = server;
