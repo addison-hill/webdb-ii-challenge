cd51c337f88d285b4b45ba6e7de11fb236ec5934
Addison Hill <adddison.hill@gmail.com>
2019-12-10 16:03:04 -0600

    All CRUD endpoints done, MVP and 2 stretch done

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const carsRouter = require("../cars/cars-router");
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use("/api/cars", carsRouter);
 
 server.get("/", (req, res) => {
   res.send("<h3>Car Dealership Database</h3>");
